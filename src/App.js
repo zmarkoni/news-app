425a08f44c962ed85c328e177d607eccf06d1835
Zoran Markovic <zoran.markovic@netcentric.biz>
2020-05-16 19:53:53 +0200

    add redux and basicFolder Store structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = (props) => {
+	console.log('App.js props: ', props);
+	return (
+		<React.Fragment>
+			<div>Zoki</div>
+		</React.Fragment>
+	);
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux'; // We use provide to wrap our React APP to connect it with Redux
+import ReduxThunk from 'redux-thunk';
 
 import './index.scss';
+import App from './App';
+
+import newsReducer from './store/reducers/topHeadlines';
+
+const rootReducer = combineReducers({
+	news: newsReducer,
+});
+
+// Create Middleware
+const logger = (store) => {
+	return (next) => {
+		return (action) => {
+			console.log('[Middleware] Dispatching', action);
+			const result = next(action);
+			console.log('[Middleware] next state', store.getState());
+			return result;
+		};
+	};
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(logger, ReduxThunk))
+);
 
 ReactDOM.render(
-	<div className="news">
-		<h1>News app</h1>
-	</div>,
+	<Provider store={store}>
+		<App />
+	</Provider>,
 	document.getElementById('root')
 );
