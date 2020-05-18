32e0e577cbd451396872074b89dfb979deea1b11
Zoran Markovic <zoran.markovic@netcentric.biz>
2020-05-18 12:47:23 +0200

    mobile nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,48 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-
+import BurgerIcon from '../../resources/icons/burger-menu.svg';
 const Navigation = () => {
+	const [navState, setNavState] = useState(false);
+	const clickHandler = () => {
+		setNavState(!navState);
+	};
+
 	return (
-		<nav role="navigation" className="nav">
-			<ul>
-				<li>
-					<NavLink
-						exact={true}
-						className="link"
-						activeClassName="is-active"
-						to="/"
-					>
-						Top News
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-						className="link"
-						activeClassName="is-active"
-						to="/categories"
-					>
-						Categories
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-						className="link"
-						activeClassName="is-active"
-						to="/search"
-					>
-						Search
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-						className="link link--disabled"
-						to="/singleArticle"
-					>
-						singleArticle
-					</NavLink>
-				</li>
-			</ul>
-		</nav>
+		<div className="nav">
+			<BurgerIcon className="burgerIcon" onClick={clickHandler} />
+
+			<nav
+				role="navigation"
+				className={`navigation navigation--${
+					navState ? 'active' : 'disabled'
+				}`}
+			>
+				<ul>
+					<li>
+						<NavLink
+							exact={true}
+							className="link"
+							activeClassName="is-active"
+							to="/"
+							onClick={clickHandler}
+						>
+							Top News
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className="link"
+							activeClassName="is-active"
+							to="/categories"
+							onClick={clickHandler}
+						>
+							Categories
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className="link"
+							activeClassName="is-active"
+							to="/search"
+							onClick={clickHandler}
+						>
+							Search
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							className="link link--disabled"
+							to="/singleArticle"
+						>
+							singleArticle
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	);
 };
 
